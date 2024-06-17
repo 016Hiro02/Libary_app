ab2c83a5799fb030d92575fcfa3d17ce8bb5f3e6
016Hiro02 <nedolepexa@gmail.com>
2024-06-17 14:06:15 +0300

    dev forms ver

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,3 +11,13 @@ urlpatterns = [
     re_path(r'^mybooks/$', views.LBBULV.as_view(), name='my-borrowed'),
     re_path(r'^libM/$', views.libM, name='libM')
 ]
+
+urlpatterns += [
+    re_path(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    re_path(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    re_path(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    re_path(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,16 @@ from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+from .forms import RenewBookForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+
+
 
 @login_required
 def index(request):
@@ -84,7 +94,38 @@ class LBBULV(LoginRequiredMixin,generic.ListView):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
 @permission_required('catalog.can_mark_returned')
-@permission_required('catalog.can_edit')
 def libM(request):
     bookinst = BookInstance.objects.filter(status__exact='o').order_by('due_back')
     return render(request, 'libM.html', context={'bookinstance_list':bookinst})
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+
+        form = RenewBookForm(request.POST)
+
+        if form.is_valid():
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            return HttpResponseRedirect(reverse('libM') )
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+    return render(request, 'book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
